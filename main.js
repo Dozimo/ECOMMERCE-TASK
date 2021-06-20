610b9b7b70b7dfcfbf96a2ce24b5ddd29a17cd01
uzomaalozie <cuealozie@gmail.com>
2021-06-20 23:25:40 +0100

    final changes

--- main.js
+++ main.js
@@ -10,4 +10,34 @@ function change() {
                     btn.innerHTML = "Login";
                 }
 }
-
+const addProductModalToggler = document.getElementById(
+    'addProductModalToggler'
+  );
+  const closeModalIcon = document.getElementById('closeModalIcon');
+  const addNewProductModal = document.getElementById('addNewProductModal');
+  const newProductForm = document.getElementById('newProductForm');
+  const addProductBtn = document.getElementById('addProductBtn');
+  
+  const hideNewProductModal = () => {
+    addNewProductModal.classList.add('hide-me');
+  };
+  const showNewProductModal = () => {
+    addNewProductModal.classList.remove('hide-me');
+  };
+  const resetNewProductForm = () => {
+    newProductForm.reset();
+  };
+  
+  //Show Add New Product Modal on click
+  addProductModalToggler.addEventListener('click', (e) => {
+    e.preventDefault();
+    showNewProductModal();
+  });
+  
+  //Close Modal and reset form on click
+  closeModalIcon.addEventListener('click', () => {
+    hideNewProductModal();
+    resetNewProductForm();
+  });
+  
+  
